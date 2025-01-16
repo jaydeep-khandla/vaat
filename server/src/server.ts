279179e78009fff5cda423f23437263118a3c6ab
Jaydeep Khandla <jaydeepkhandla.work@gmail.com>
2025-01-16 18:21:16 +0530

    feat: :sparkles: Add dotenv for environment variable management; configure server to use PORT from .env

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
-import httpServer from "./app";
+import httpServer from './app';
+require('dotenv').config();
 
-import Logger from "./utils/logger";
-import { socketConnection } from "./utils/socket";
+import Logger from './utils/logger';
+import { socketConnection } from './utils/socket';
 
-const logger = new Logger("server");
+const logger = new Logger('server');
 
 // Start the server
-const port = 8000;
+const port = process.env.PORT || 8000;
 httpServer.listen(port, function serverListening() {
   logger.info(`Server started at http://localhost:${port}`);
   socketConnection();
